abbb3a28fb05c0bf49baba1f23d026304ab2fd18
MauroTheCreator <maurotorresdev@gmail.com>
2021-06-03 05:22:31 -0300

    removendo comentário obsoleto

--- tictactoe.go
+++ tictactoe.go
@@ -1,14 +1,5 @@
 package main
 
-// TODO:
-// 1. Implementar função pra pegar as coordenadas | DONE
-// 2. Implementar função pra pegar as opções      | DONE
-// 3. Implementar validação do Grid               | DONE
-//    - validar todas as formas possíveis         | DONE
-//    - sair do jogo se der TOE                   | DONE
-//    - main loop mais bem feito                  | DONE
-// 4. Checar se já está marcado                   | DONE
-
 import (
     "fmt"
     . "tic-tac-toe/table"
@@ -57,5 +48,4 @@ func main() {
 func ClearScreen() {
     fmt.Print("\033[H\033[2J")
 }
-
 
